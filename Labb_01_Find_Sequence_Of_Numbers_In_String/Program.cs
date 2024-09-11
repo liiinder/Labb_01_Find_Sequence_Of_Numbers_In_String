3fc9d8ad1b1fab8b8fd221ccbbe175cf07ea4aec
liiinder <linderkristoffer@hotmail.com>
2024-09-11 18:27:52 +0200

    Final version of Labb 1 with loops / without substrings

--- Labb_01_Find_Sequence_Of_Numbers_In_String/Program.cs
+++ Labb_01_Find_Sequence_Of_Numbers_In_String/Program.cs
@@ -1,14 +1,11 @@
-﻿//Console.Write("Skriv in en text: ");
-//string input = Console.ReadLine();
-//SumOfAllSeqNumbers(input);
+﻿Console.Write("Skriv in en text: ");
+string input = Console.ReadLine();
+SumOfSeqNumbers(input);
 
-///  Takes a string and prints the sequenses where there is 
-///  a repeted number with numbers inbetween but not any other characters
-///  and then sums the matching sequenses together.
-static void SumOfAllSeqNumbers(string input)
+static void SumOfSeqNumbers(string input)
 {
     Console.WriteLine();
-    ulong sumOfAll = 0;
+    ulong result = 0;
 
     for (int i = 0; i < input.Length; i++)
     {
@@ -23,76 +20,27 @@ static void SumOfAllSeqNumbers(string input)
 
             currentSequence += currentChar;
 
-            if (startChar == currentChar && j != i)
+            if (j != i && startChar == currentChar)
             {
-                sumOfAll += ulong.Parse(currentSequence);
+                result += ulong.Parse(currentSequence);
                 HighlightedPrint(input, i, j);
                 break;
             }
         }
     }
-    Console.WriteLine($"\nTotal = {sumOfAll}");
+    Console.WriteLine($"\nTotal = {result}");
 }
 
-/// Takes a string and highlights the text in red from startIndex to endIndex
-//static void HighlightedPrint(string input, int startIndex, int endIndex, string color = "Green")
-//{
-//    for (int i = 0; i < input.Length; i++)
-//    {
-
-//        if (i == startIndex)
-//        {
-//            if (color == "Green") Console.ForegroundColor = ConsoleColor.Green;
-//            if (color == "Red") Console.ForegroundColor = ConsoleColor.Red;
-//        }
-//        Console.Write(input[i]);
-
-//        if (i == endIndex) Console.ForegroundColor = ConsoleColor.Gray;
-//    }
-//    Console.WriteLine();
-//}
-
+/// Prints a string and highligts characters between startIndex and endIndex in green.
 static void HighlightedPrint(string input, int startIndex, int endIndex)
 {
-    endIndex++;
-    int length = endIndex - startIndex;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (i == startIndex) Console.ForegroundColor = ConsoleColor.Green;
 
-    string start = input.Substring(0, startIndex);
-    string middle = input.Substring(startIndex, length);
-    string end = input.Substring(endIndex);
+        Console.Write(input[i]);
 
-    Console.Write(start);
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.Write(middle);
-    Console.ForegroundColor = ConsoleColor.Gray;
-    Console.Write(end + "\n");
+        if (i == endIndex) Console.ResetColor();
+    }
+    Console.WriteLine();
 }
-
-/// Checking a character with ascii/unicode conversion if its outside the scope of the numbers 0-9
-/// Without this method it will allow lots of other characters to be seen as numbers seen on this page.
-/// https://stackoverflow.com/questions/2866738/whats-the-deal-with-char-getnumericvalue
-//static bool CharIsNotDigit(char c) => ((c - '0') < 0 || (c - '0') > 9);
-
-// Test cases
-
-string preset = "29535123p48723487597645723645";
-Console.WriteLine("29535123p48723487597645723645");
-Console.WriteLine("preset.Lenght " + preset.Length);
-SumOfAllSeqNumbers(preset);
-Console.WriteLine("\n--------------------------------------------\n");
-
-//string asciiDumb = "295८35123៦487234875976፫45723645";
-//SumOfAllSeqNumbers(asciiDumb);
-//Console.WriteLine("\n--------------------------------------------\n");
-/// Would have worked to not use my method and use TryParse or char.IsAsciiDigit instead.
-
-
-//int padding = 45;
-//Console.WriteLine("char.IsDigit('८') = ".PadLeft(padding) + char.IsDigit('८'));
-//Console.WriteLine("char.IsAsciiDigit('८') = ".PadLeft(padding) + char.IsAsciiDigit('८'));
-//Console.WriteLine("char.IsAsciiDigit('८') = ".PadLeft(padding) + char.IsAscii('८'));
-//Console.WriteLine("char.GetNumericValue('८') = ".PadLeft(padding) + char.GetNumericValue('८'));
-//Console.WriteLine("Int32.TryParse(\"८\", out int result1) = ".PadLeft(padding) + Int32.TryParse("८", out int result1));
-//Console.WriteLine("Int32.TryParse(\"८\", out int result1) = ".PadLeft(padding) + Int32.TryParse('८'.ToString(), out int result4));
-//Console.WriteLine("Int32.TryParse(\"B\", out int result2) = ".PadLeft(padding) + Int32.TryParse("B", out int result2));
-//Console.WriteLine("Int32.TryParse(\"123\", out int result3) = ".PadLeft(padding) + Int32.TryParse("123", out int result3));
