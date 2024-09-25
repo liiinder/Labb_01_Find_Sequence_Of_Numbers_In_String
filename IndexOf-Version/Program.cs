46cb46a10724539738f5b6ed3b4fd5087cad8530
liiinder <linderkristoffer@hotmail.com>
2024-09-25 14:15:21 +0200

    Uppdaterat indexOf med en steg för steg kommentar.

--- IndexOf-Version/Program.cs
+++ IndexOf-Version/Program.cs
@@ -1,40 +1,61 @@
-﻿// Labb 1 version "oläsligt..."
-static void Labb1Short(string input)
+﻿Console.Write("Skriv in en text: ");
+string input = Console.ReadLine();
+ulong sumOfAll = 0;
+for (int i = 0; i < input.Length - 1; i++)
 {
-    ulong result = 0;
-    for (int i = 0; i < input.Length; i++)
-    {
-        int seqLength = input.Substring(i + 1).IndexOf(input[i]) + 2; 
-        if (seqLength == 1) continue; 
-        if (!ulong.TryParse(input.Substring(i, seqLength), out ulong currSum)) continue;
-        result += currSum;
-        Console.Write("\n" + input.Substring(0, i));
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write(input.Substring(i, seqLength));
-        Console.ResetColor();
-        Console.Write(input.Substring(i + seqLength));
-    }
-    Console.WriteLine($"\n\nTotal = {result}");
+    int reminderStart = input[(i + 1)..].IndexOf(input[i]) + i + 2;
+    if (reminderStart - i == 1 || !ulong.TryParse(input[i..reminderStart], out ulong currSum)) continue;
+    sumOfAll += currSum;
+    Console.Write("\n" + input[..i]);
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.Write(input[i..reminderStart]);
+    Console.ResetColor();
+    Console.Write(input[reminderStart..]);
 }
+Console.WriteLine($"\n\nTotal = {sumOfAll}");
 
-static string askUser()
-{
-    Console.Write("Skriv in en text: ");
-    return Console.ReadLine();
-}
+/* 
+    Steg för steg...
 
-//Labb1Short(askUser());
-Labb1Short("29535123p48723487597645723645");
+--- Loop --------------------------------------------------------------------------------------------------------------------
 
-// Loopa tecken för tecken
+    En loop som går igenom alla tecken och för varje tecken så vill vi leta efter ett matchande tecken
+    
+    Exempel:
+    --------
+    string input = "29535123p48723487597645723645";
+    Om i = 2 så returnerar input[(i + 1)..] substrängen 35123p48723487597645723645
 
-// Kolla strängen från nästa tecken och till slutet efter ett matchande tecken
-// i = 2 -> substring -> 35123p48723487597645723645 -> indexOf(5) -> 1 -> + 2 = 3 
+    Sedan kollar vi om det finns en matchande siffra i denna substräng med
+    indexOf(5) -> vilket kommer returnera 1 ( 3[5]123p48723487597645723645 )
+    Om det inte finns en träff returneras -1
 
-// Är längden 1 så betyder det att raden ovan returnerade -1 (+2) och det inte finns en matchning
+    Vi är ute efter indexet i orginal strängen på tecknet efter matchningen
+        29535[1]23p48723487597645723645 -> index = 5
 
-// Parsa substringen och spara ev summa i currSum.
-// Om det finns bokstäver i substrängen "continue;"
-// då vi inte vill spara och skriva ut denna
+    Då första tecknet i substrängen har index (i + 1) i orginalsträngen
+    så behöver vi lägga till (i + 1) till vårat svar för att få indexet på matchningen i orginalsträngen
+    MEN eftersom vi vill ha tecknet efter lägger vi på (i + 2).
 
-// Spara delresultatet och skriv ut raden
+--- IF-sats -----------------------------------------------------------------------------------------------------------------
+
+    Kollar efter 2 saker:
+    ---------------------
+    1. (reminderStart - i == 1) -> längden på den matchande substrängen, är den 1 betyder det att vi inte hittat någon matchning
+    2. TryParse kommer kolla om strängen går att parsa till en long och returnera true/false och spara i variabeln "currSum",
+       går det inte parsa så returneras true (då vi använder ! före och vänder på svaret).
+    
+    Om något av dessa är sant så går vi vidare i loopen med ( continue; )
+
+--- Utskrift ----------------------------------------------------------------------------------------------------------------
+
+    input[..i]               - Substring från start till tecknet innan i (sista är exkluderande)
+        29
+
+    input[i..reminderStart]  - Substring från i till tecknet innan reminderStart
+        535
+
+    input[reminderStart..]   - Substring från reminderStart till slutet av strängen.
+        123p48723487597645723645
+
+*/
